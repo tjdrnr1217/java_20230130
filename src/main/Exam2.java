cbeba5b8d93ff3d53af3eed1b2a584b1a247310d
tjdrnr1217 <tjdrnr1217@nate.com>
2023-01-30 16:44:05 +0900

    20230130
    변수,조건문,반복문

--- src/main/Exam2.java
+++ src/main/Exam2.java
@@ -77,7 +77,7 @@ public class Exam2 {
 			System.out.println("7 * " + i + "=" + (7*i));
 		}
 		
-		for(i=1;i<=9;i++) {
+		for(i=1;i<=9;i++ ) {
 			for(j=1;j<=9;j++) {
 				System.out.println(i+"*"+j+"="+(i*j));
 			}
